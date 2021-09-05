f7651488098690d0bec8456cc0886c75b139d5fe
kolev <kolevtestsoftware@gmail.com>
2021-09-06 00:44:59 +0300

    Add last changes and Readme.MD

--- src/main/java/simpleParser/services/parser/splitter/GenericInvoiceSplitter.java
+++ src/main/java/simpleParser/services/parser/splitter/GenericInvoiceSplitter.java
@@ -2,11 +2,6 @@ package simpleParser.services.parser.splitter;
 
 import simpleParser.models.Invoice;
 
-import java.util.List;
-import java.util.Map;
-
 public interface GenericInvoiceSplitter {
-    void split(Map<String, List<Invoice>> splitInvoices, Invoice invoice);
-
     String getFileName(Invoice invoice);
 }
